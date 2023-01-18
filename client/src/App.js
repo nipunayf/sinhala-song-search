0caad9b5f6081771087c9b13b811fff3fca94ec8
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-19 01:00:18 +0530

    Implement pagination

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,22 @@ import {
 import SearchBar from "./containers/SearchBar";
 import Filter from "./containers/Filter";
 import Results from "./containers/Results";
+import {useState} from "react";
 
 function App() {
+    const [data, setData] = useState([]);
+    const [aggs, setAggs] = useState([])
+    const [hits, setHits] = useState(0);
+    const [query, setQuery] = useState('');
+
     return (
         <ChakraProvider>
-            <HStack p={8} align={'center'}>
-                {/*<Filter/>*/}
-                {/*<Spacer/>*/}
+            <HStack p={8} align={'top'}>
+                <Filter genre_count={aggs} hits={hits} setData={setData} query={query}/>
+                <Spacer/>
                 <VStack w={'100%'} pr={6} spacing={8}>
-                    <SearchBar/>
-                    <Results/>
+                    <SearchBar setData={setData} setAggs={setAggs} setHits={setHits} setQuery={setQuery}/>
+                    <Results data={data}/>
                 </VStack>
             </HStack>
         </ChakraProvider>
--- client/src/containers/Results.jsx
+++ client/src/containers/Results.jsx
@@ -1,28 +1,33 @@
-import {Badge, Box, HStack, Text, VStack} from "@chakra-ui/react";
+import {HStack, Text, VStack} from "@chakra-ui/react";
 
 const SingeLine = ({keyValue, value}) => (<HStack>
-    <Text>{keyValue}:</Text>
-    <Text>{value}</Text>
+    <Text as={'b'}>{keyValue}:</Text>
+    <Text><div dangerouslySetInnerHTML={{ __html: value }} /></Text>
 </HStack>);
 
-function LyricsCard() {
+function LyricsCard({line, source, target, meaning, song, artist, composer, writer, genre}) {
     return (
         <VStack borderWidth='1px' borderRadius='lg' overflow='hidden' boxShadow={'md'} w={980} align={'left'} p={2}>
-            <Text>Sinduwa</Text>
-            <SingeLine keyValue={"Gayanaya"} value={"Amaradewa"}/>
-            <SingeLine keyValue={"Compose eka"} value={"Amaradewa"}/>
-            <SingeLine keyValue={"Genres:"} value={"Amaradewa"}/>
-            <SingeLine keyValue={"Writer:"} value={"Amaradewa"}/>
+            <SingeLine keyValue={"වැකිය"} value={line}/>
+            <SingeLine keyValue={"උපමාව් "} value={source}/>
+            <SingeLine keyValue={"උපමේය"} value={target}/>
+            <SingeLine keyValue={"අර්ථය"} value={meaning}/>
+            <SingeLine keyValue={"සින්දුව"} value={song}/>
+            <SingeLine keyValue={"ගායකයා "} value={artist}/>
+            <SingeLine keyValue={"නිර්මාපකයෙක්"} value={composer}/>
+            <SingeLine keyValue={"ගත්කරු "} value={writer}/>
+            <SingeLine keyValue={"වර්ගය"} value={genre}/>
         </VStack>
     )
 }
 
-export default function Results() {
+export default function Results({data}) {
     return (
         <VStack>
-            <LyricsCard/>
-            <LyricsCard/>
-            <LyricsCard/>
+            {data.map(entry => <LyricsCard line={entry.line} source={entry.source} target={entry.target}
+                                           meaning={entry.meaning} song={entry.title} artist={entry.artist.join(", ")}
+                                           composer={entry.composer.join(',')} writer={entry.writer.join(', ')}
+                                           genre={entry.genre.join(', ')}/>)}
         </VStack>
     );
 }
